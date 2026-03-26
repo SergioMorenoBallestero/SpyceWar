5eedb0e75a3728edd3e0a0c8d17cc0360c66db39
SergioMorenoBallestero <100582747@alumnos.uc3m.es>
2026-03-26 23:52:57 +0100

    Ship class: separated responsibilities between update_acceleration() and update_velocity().

--- source_files/ship.py
+++ source_files/ship.py
@@ -12,6 +12,7 @@ class Ship:
         self.velocity = Vector2()
         self.acceleration = Vector2()
 
+
     def move(self, keyboard: list[int]):
         """ Updates the position of the ship based on keyboard input.
         **This function must do a check to ensure the ship doesn't go OOB**"""
@@ -19,17 +20,10 @@ class Ship:
         self.__update_velocity()
         self.position += self.velocity
 
-    def __update_velocity(self):
-        """ Updates the velocity by adding the value of acceleration
-         **This function must do a check to ensure the cap acceleration isn't reached**"""
-        # this feels weird... I should check it later prolly
-        if (self.velocity + self.acceleration).length() <= constants.VELOCITY_CAP:
-            self.velocity += self.acceleration
 
     def __update_acceleration(self, keyboard: list[int]):
         """ Updates the acceleration value by comparing the input to the velocity """
-        # if there is some null input value
-        if keyboard[0] == 0 or keyboard[0] == 0:
+        if keyboard[0] == 0 or keyboard[0] == 0: # if there is some null input value
             if keyboard[0] == 0 and self.velocity.x != 0: # case 1: x input is 0, and x velocity is not 0
                 self.acceleration.x = self.velocity.x
             if keyboard[1] == 0 and self.velocity.y != 0: # case 2: y input is 0, and y velocity is not 0
@@ -43,12 +37,26 @@ class Ship:
         # adjust the acceleration scale
         self.__update_accel_scale()
 
+
     def __update_accel_scale(self):
         """ Scales the acceleration depending on the velocity cap """
-        # scale the acceleration vector (default case, the cap is not reached)
-        self.acceleration.scale(constants.ACCELERATION)
         # check whether the new velocity will be greater than the current one
-        if (self.velocity + self.acceleration).length() > constants.VELOCITY_CAP:
-            # the scale of the acceleration should be the difference between the cap and the current velocity
-            difference = constants.VELOCITY_CAP - self.velocity.length()
-            self.acceleration.scale(difference / constants.ACCELERATION)
+        if self.velocity.length != constants.VELOCITY_CAP:
+            # scale the acceleration vector
+            self.acceleration.scale(constants.ACCELERATION)
+            if (self.velocity + self.acceleration).length() > constants.VELOCITY_CAP:
+                # the scale of the acceleration should be the difference between the cap and the current velocity
+                difference = constants.VELOCITY_CAP - self.velocity.length()
+                self.acceleration.scale(difference / constants.ACCELERATION)
+
+
+    def __update_velocity(self):
+        """ Updates the velocity by adding the value of acceleration, or forcing it to its max value """
+        # the first if statement is there to maintain acceleration and preserving direction changes
+        if self.velocity.length() == constants.VELOCITY_CAP: # special case: velocity is already at its max value
+            # set the velocity to the normalized acceleration (if this triggered, acceleration has not been scaled)
+            self.velocity = Vector2(self.acceleration.x,self.acceleration.y)
+            # scale it to its maximum value
+            self.velocity.scale(constants.VELOCITY_CAP)
+        else:
+            self.velocity += constants.ACCELERATION
